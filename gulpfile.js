9740d2fb595d5e1626923ec5e36acccf73d14bd3
rifa712 <rifaalghan@gmail.com>
2022-07-15 13:11:27 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss')
+
+function buildStyles() {
+  return src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(purgecss({ content: ['*.html'] }))
+    .pipe(dest('css'))
+}
+
+// watch changes in/on files specified
+function watchTask() {
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
